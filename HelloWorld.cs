4ef3b67156742409a660a9817f723a00e051a07e
Aleksey_Kushnarev <77682713+escaleksey@users.noreply.github.com>
2024-11-20 20:03:08 +0500

    Add files via upload
    Добавил скрипт, приветсвующий мир!

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // 1 раз в начале игры
+    void Start()
+    {
+        print("Hello world!");
+    }
+    // ctrl + s
+    // Update is called once per frame
+    // ¬ыполн€етс€ много раз
+    void Update()
+    {
+        
+    }
+}
